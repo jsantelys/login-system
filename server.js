55a46dabf9119e21f29273260bc5d43feb79648b
Javier Santelys <javiersantelys@gmail.com>
2022-03-22 17:29:10 -0300

    First setup of project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
+app.get('login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.listen(3000)
